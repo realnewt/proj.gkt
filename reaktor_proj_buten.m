986862b83d87e20ee5a26b02caa9c6f2182bc249
realnewt <ialmyren@gmail.com>
2020-04-09 18:38:38 +0200

    startfilen samt arbetsfilen för reaktorn

--- reaktor_proj_buten.m
+++ reaktor_proj_buten.m
@@ -4,24 +4,32 @@
 % Reaction:        A + 2B -> P
 % Rate Epression:  r = k * CA * CB^2 / (1 + K * CP)
 % 
-clear all;
+clear 
+clc
 %Set values of constants
-NA0=10;  NB0=50;  NP0=0; T0=175+273;  % initial number of moles of A, B and P
-k=0.005;  K=0.5;           % kinetic parameters
-V=10;                     % volume of reactor
+FA0=10;  FB0=50; T0=850+273;  % initial number of moles of A, B and P
+R=8.314;    T=T0;       Ea=141e3;
+k=0.0596;  K1=22.90; K2=7.56 ; Ke=2.1e7*exp(-122 /(R*T));           % kinetic par. k @550 K1&K2 bar Ke scale with temp also kJ/mol bar
+                     % volume of reactor
+V=0
+g=0;
+T=550+273.15;   A=k/(exp(-Ea/(R*T)));  
+
+
 %Start and Stop times
-t_start=0;   % start value for t
-t_final=100; % final value for t
-%[t_out,U_out] = ode45(@ode_function,[t_range],[initial_values],
-[options],constants)
-[t U]=ode15s(@ode_eq,[t_start t_final],[NA0 NB0 NP0 T0],[],k,K,V);
+V_start=0;   % start value for t
+V_final=100; % final value for t
+%[t_out,U_out] = ode45(@ode_function,[t_range],[initial_values], [options],constants)
+[V, U]=ode15s(@ode_eq,[V_start, V_final],[FA0 FB0 T0],[],k,Ke,K1,K2,V,A,Ea,R);
 %extract NA, NB and NP from U
-NA=U(:,1);  NB=U(:,2);  NP=U(:,3);
+FA=U(:,1);  FB=U(:,2); 
 %calculate concentrations
-CA=NA/V;  CB=NB/V;  CP=NP/V;
+
+
 %calculate conversion of A
-XA=(NA0-NA)./NA0;
-%plot moles versus time
+XA=(FA0-FA)./FA0
+
+%% plot moles versus time
 figure(1)
 plot(t,NA,t,NB,t,NP)
 xlabel('t')
