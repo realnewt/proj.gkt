75a98400130e5b44e1f73eed91a0248466065f7b
realnewt <ialmyren@gmail.com>
2020-04-09 11:12:35 +0200

    enkla ändringar

--- ode_eq.m
+++ ode_eq.m
@@ -1,17 +1,17 @@
-function dUdW=ode_eq(W,U,k,K,V)
+function dUdW=ode_eq(W,U,k,Ke,K1,K2,V,A,Ea,R)
 % function file containing differential equations
 %extract NA, NB and NP from U
 FA=U(1);  FB=U(2);  T=U(3);
 
 %calculate concentrations preassure
-Pa=;    Pb=;      PH=;
+Pa=0;    Pb=0;      PH=0;
 %calculate reaction rate
 k=A*exp(-Ea/R/T);
-r=(k(Pa-(PbPH/Ke)))/(1+K1*Pb*PH^.5+(K2PH)^.5);
+r=(k*(Pa-(Pb*PH/Ke)))/(1+K1*Pb*PH^.5+(K2*PH)^.5);
 %differential equations
 dFAdw=-r;
 dFBdW=r;
-dTdW=0;
+dTdW=0;      %  (r*(-dH)+Q)/(Fa*Cpa+Fb*Cpb);
 %Assemble Differential Equations
 dUdW=[dFAdw
     dFBdW
