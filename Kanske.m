871a2bde38f1b625fc49b66314cd58d73f7a3185
realnewt <ialmyren@gmail.com>
2020-04-11 16:25:10 +0200

    test av olika FA, FB och W
    verkar lovande

--- Kanske.m
+++ Kanske.m
@@ -1,8 +1,11 @@
 % A=butan B=buten H=väte W=vatten
-Wtot=1000; %kg cat
+clear
+clc
+clf
+Wtot=20000; %kg cat
 XA_start=0;
-T0=950; %K
-FA0=54; %mol/s
+T0=1000; %K
+FA0=100; %mol/s
 FB0=0;
 FW0=10*FA0;
 
@@ -14,11 +17,44 @@ CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0
 XA=Y(:,1); T=Y(:,2);
 
 figure(1)
-plot(W,XA)
+plot(W,XA, 'blue'), hold on
 xlabel('amount catalyst(kg)')
 ylabel('XA')
 
 figure(2)
-plot(T,XA)
+plot(T,XA, 'blue'), hold on
 xlabel('Temp(K)')
 ylabel('XA')
+
+
+%%
+
+Wtot=20000; %kg cat
+XA_start=0;
+T0=1000; %K
+FA0=100; %mol/s
+FB0=.5*FA0;
+FW0=10*FA0;
+
+HR=116.3e3; %J/mol
+P=1; %bar
+CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0.009274 -1.3813e-05 7.645e-09; 32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
+[W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
+
+XA=Y(:,1); T=Y(:,2);
+
+figure(1)
+plot(W,XA, 'r')
+xlabel('amount catalyst(kg)')
+ylabel('XA')
+
+figure(2)
+plot(T,XA, 'r')
+xlabel('Temp(K)')
+ylabel('XA')
+
+
+
+
+
+
