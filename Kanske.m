f3b2581d9e2836b6415c3c4e65f93faf853618cb
leylabeckerman <63494443+leylabeckerman@users.noreply.github.com>
2020-04-11 11:21:58 +0200

    Add files via upload

---
+++ Kanske.m
@@ -0,0 +1,24 @@
+% A=butan B=buten H=väte W=vatten
+Wtot=1000; %kg cat
+XA_start=0;
+T0=950; %K
+FA0=54; %mol/s
+FB0=0;
+FW0=10*FA0;
+
+HR=116.3e3; %J/mol
+P=1; %bar
+CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0.009274 -1.3813e-05 7.645e-09; 32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
+[W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
+
+XA=Y(:,1); T=Y(:,2);
+
+figure(1)
+plot(W,XA)
+xlabel('amount catalyst(kg)')
+ylabel('XA')
+
+figure(2)
+plot(T,XA)
+xlabel('Temp(K)')
+ylabel('XA')
---
+++ ode_func.m
@@ -0,0 +1,28 @@
+function dYdW=ode_func(W,Y,HR,P,CP,FA0,FB0,FW0)
+%function file containing differential equations
+
+XA=Y(1); T=Y(2);
+
+R=8.314; %J/K/mol
+K1=22.90; %bar^-1.5
+K2=7.56; %/bar
+Ea=141e3; %J/mol
+
+FA=(FA0)*(1-XA); FB=FB0+FA0*XA; FH=FA0*XA; FW=FW0;
+Ftot=FA+FB+FH+FW;
+
+PA=FA/Ftot*P; PB=FB/Ftot*P; PH=FH/Ftot*P;
+k=0.0596*exp((Ea/R).*(1./(550+273)-1./T)); %mol/(kg cat)/s/bar
+Ke=2.1*10^7*exp(-122/(R*T)); %bar
+
+cpA=@(T)CP(1,1)+CP(1,2).*T+CP(1,3).*T.^2+CP(1,4).*T.^3;
+cpB=@(T)CP(2,1)+CP(2,2).*T+CP(2,3).*T.^2+CP(2,4).*T.^3;
+cpH=@(T)CP(3,1)+CP(3,2).*T+CP(3,3).*T.^2+CP(3,4).*T.^3;
+cpW=@(T)CP(4,1)+CP(4,2).*T+CP(4,3).*T.^2+CP(4,4).*T.^3;
+
+delHr=HR-integral(cpA,298,T)+integral(cpB,298,T)+integral(cpH,298,T);
+
+r=(k*(PA-(PB*PH)/Ke))/(1+K1*PB*PH^0.5+(K2*PH)^0.5);
+
+dYdW=[r/FA0 %mole balance
+    (r*(-delHr))/(FA*cpA(T)+FB*cpB(T)+FH*cpH(T)+FW*cpW(T))]; %heat balance
