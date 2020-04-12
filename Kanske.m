39fed99b67e1ea1f762fec43c7f1926488b2266e
realnewt <ialmyren@gmail.com>
2020-04-12 12:03:15 +0200

    testar olika FB samt antalet reaktorer
    testar hur många reaktorer med 2000 kg W i. jag har gjort detta med en while loop på ett ganska primitivt sätt

--- Kanske.m
+++ Kanske.m
@@ -2,20 +2,45 @@
 clear
 clc
 clf
-Wtot=20000; %kg cat
+e=0;
+Wtot=2000; %kg cat
 XA_start=0;
-T0=1000; %K
+T0=850; %K
 FA0=100; %mol/s
 FB0=0;
 FW0=10*FA0;
 
 HR=116.3e3; %J/mol
 P=1; %bar
-CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0.009274 -1.3813e-05 7.645e-09; 32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
+CP=[1.39 0.3847 -1.846e-04 2.895e-08;
+    16.05 0.2804 -1.091e-04 9.098e-09;
+    27.14 0.009274 -1.3813e-05 7.645e-09; 
+    32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
+
+while XA_start-0.95<1e-2
 [W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
 
 XA=Y(:,1); T=Y(:,2);
 
+plot(W,XA, ''), hold on
+xlabel('amount catalyst(kg)')
+ylabel('XA')
+
+XA_start=max(XA);
+FA=(FA0)*(1-XA_start); FB=FB0+FA0*XA_start; FH=FA0*XA_start; FW=FW0;
+Ftot=FA+FB+FH+FW;
+FA0=FA; %mol/s
+FB0=FB;
+FW0=10*FA0;
+e=e+1;
+end
+legend('1','2','3','4','5','6','7','8','9')
+e
+
+
+
+
+%%
 figure(1)
 plot(W,XA, 'blue'), hold on
 xlabel('amount catalyst(kg)')
@@ -28,12 +53,13 @@ ylabel('XA')
 
 
 %%
-
-Wtot=20000; %kg cat
+clf
+for i=1:10
+Wtot=1000; %kg cat
 XA_start=0;
 T0=1000; %K
 FA0=100; %mol/s
-FB0=.5*FA0;
+FB0=(i*0.1)*FA0;
 FW0=10*FA0;
 
 HR=116.3e3; %J/mol
@@ -44,16 +70,18 @@ CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0
 XA=Y(:,1); T=Y(:,2);
 
 figure(1)
-plot(W,XA, 'r')
+plot(W,XA, ''), hold on
 xlabel('amount catalyst(kg)')
 ylabel('XA')
 
-figure(2)
-plot(T,XA, 'r')
-xlabel('Temp(K)')
-ylabel('XA')
-
-
+%figure(2)
+%plot(T,XA, ''),hold on
+%xlabel('Temp(K)')
+%ylabel('XA')
+end
+%legend('1','2','3','4','5','6','7','8','9','10')
+figure(1)
+legend('1','2','3','4','5','6','7','8','9','10')
 
 
 
