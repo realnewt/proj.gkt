dedd3e629a7a905d8acafdc56f6c39ec1ba3e43d
realnewt <ialmyren@gmail.com>
2020-04-13 14:09:07 +0200

    lite allmänt fix
    for loop borttagen, plots tystade

--- Kanske.m
+++ Kanske.m
@@ -17,7 +17,7 @@ CP=[1.39 0.3847 -1.846e-04 2.895e-08;
     27.14 0.009274 -1.3813e-05 7.645e-09; 
     32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
 
-while XA_start-0.95<1e-3
+while XA_start-0.95<1e-2
 [W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
 
 XA=Y(:,1); T=Y(:,2);
@@ -34,51 +34,22 @@ FB0=FB;
 FW0=10*FA0;
 e=e+1;
 leg(e,:)= "reaktor "+ e;
+
 end
 legend(leg)
-e
-
-figure(1)
-plot(W,XA, 'blue'), hold on
-xlabel('amount catalyst(kg)')
-ylabel('XA')
-%% 
-figure(2)
-plot(T,XA, 'blue'), hold on
-xlabel('Temp(K)')
-ylabel('XA')
+disp("reaktorer: "+e)
 
 
-%%
-clf
-for i=1:10
-Wtot=1000; %kg cat
-XA_start=0;
-T0=1000; %K
-FA0=100; %mol/s
-FB0=(i*0.1)*FA0;
-FW0=10*FA0;
-
-HR=116.3e3; %J/mol
-P=1; %bar
-CP=[1.39 0.3847 -1.846e-04 2.895e-08; 16.05 0.2804 -1.091e-04 9.098e-09; 27.14 0.009274 -1.3813e-05 7.645e-09; 32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
-[W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
-
-XA=Y(:,1); T=Y(:,2);
-
-figure(1)
-plot(W,XA, ''), hold on
-xlabel('amount catalyst(kg)')
-ylabel('XA')
+%figure(1)
+%plot(W,XA, ''), hold on
+%xlabel('amount of catalyst(kg)')
+%ylabel('XA')
 
 %figure(2)
-%plot(T,XA, ''),hold on
+%plot(T,XA, 'blue'), hold on
 %xlabel('Temp(K)')
 %ylabel('XA')
-end
-%legend('1','2','3','4','5','6','7','8','9','10')
-figure(1)
-legend('1','2','3','4','5','6','7','8','9','10')
+
 
 
 
