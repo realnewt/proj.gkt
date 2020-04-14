103845be20f68484d8f753e1d10f1e7b7056010e
leylabeckerman <63494443+leylabeckerman@users.noreply.github.com>
2020-04-14 18:36:28 +0200

    Update Kanske.m

--- Kanske.m
+++ Kanske.m
@@ -1,13 +1,12 @@
-% A=butan B=buten H=väte W=vatten
 clear
 clc
 clf
 e=0;
-Wtot=2000; %kg cat
+Wtot=500; %kg cat
 XA_start=0;
-T0=850; %K
-FA0=100; %mol/s
-FB0=0;
+T0=950; %K
+FA0=54; %mol/s
+FB0=0.5;
 FW0=10*FA0;
 
 HR=116.3e3; %J/mol
@@ -17,13 +16,19 @@ CP=[1.39 0.3847 -1.846e-04 2.895e-08;
     27.14 0.009274 -1.3813e-05 7.645e-09; 
     32.24 0.001924 1.055e-05 -3.596e-09]; %matris med alla CP konstanter J/mol/K
 
-while XA_start-0.95<1e-2
+while XA_start-0.89<1e-4
 [W,Y]=ode15s(@(W,Y)ode_func(W,Y,HR,P,CP,FA0,FB0,FW0),[0 Wtot],[XA_start T0]);
 
 XA=Y(:,1); T=Y(:,2);
 
+figure(1)
 plot(W,XA, ''), hold on
-xlabel('amount catalyst(kg)')
+xlabel('amount of catalyst(kg)')
+ylabel('XA')
+
+figure(2)
+plot(T,XA, ''), hold on
+xlabel('Temp(K)')
 ylabel('XA')
 
 XA_start=max(XA);
@@ -36,19 +41,11 @@ e=e+1;
 leg(e,:)= "reaktor "+ e;
 
 end
+figure(1)
 legend(leg)
+figure(2)
+legend(leg,'location','northwest')
 disp("reaktorer: "+e)
-
-
-%figure(1)
-%plot(W,XA, ''), hold on
-%xlabel('amount of catalyst(kg)')
-%ylabel('XA')
-
-%figure(2)
-%plot(T,XA, 'blue'), hold on
-%xlabel('Temp(K)')
-%ylabel('XA')
 
 
 
