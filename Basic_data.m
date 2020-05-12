0f896fc1302027865c553d153d892a3edcf7fc9b
realnewt <ialmyren@gmail.com>
2020-05-12 15:02:19 +0200

    Delete Basic_data.m

--- Basic_data.m
+++
@@ -1,9 +0,0 @@
-function a=Basic_data(F_mol,M,T_in,CP)      %Calculates Cp for each component
-
-a=F_mol*M;
-
-CP_matrix=[Cp(T_in,CP(1,1),CP(1,2),CP(1,3),CP(1,4),M(1));
-           Cp(T_in,CP(2,1),CP(2,2),CP(2,3),CP(2,4),M(2));
-           Cp(T_in,CP(3,1),CP(3,2),CP(3,3),CP(3,4),M(3));
-           Cp(T_in,CP(4,1),CP(4,2),CP(4,3),CP(4,4),M(4))];
-end
